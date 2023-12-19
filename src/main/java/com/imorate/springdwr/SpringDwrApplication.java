7309ed1d3650a39187fcbcf279f64e8f660f8a67
Amir Mohammad Hl <dev.imorate@gmail.com>
2023-12-19 21:31:00 +0330

    Initial spring boot commit

---
+++ src/main/java/com/imorate/springdwr/SpringDwrApplication.java
@@ -0,0 +1,13 @@
+package com.imorate.springdwr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDwrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDwrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imorate/springdwr/SpringDwrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imorate.springdwr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDwrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
